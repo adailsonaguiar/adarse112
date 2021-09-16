474b2bcf2a27ab4f73edbe02b0a1726283d200e1
adailsonaguiar <adailsonacj@live.com>
2021-09-16 12:57:40 -0300

    refact player

--- scripts/maps.js
+++
@@ -1,8 +0,0 @@
-let map;
-
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -10.250947493382137, lng: -48.323432254618986 },
-    zoom: 17,
-  });
-}
